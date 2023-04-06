6db57a40f2e4d27b11fd81257dc07e088ddf80a9
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-04-06 09:22:53 -0600

    new graphs

--- components/3dplot.js
+++ components/3dplot.js
@@ -1,39 +1,47 @@
-d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/3d-scatter.csv', function(err, rows){
-function unpack(rows, key) {
-	return rows.map(function(row)
-	{ return row[key]; });}
+ getrandom=(num, mul)=> {
+  var value = [];
+  for (i = 0; i <= num; i++) {
+    var rand = Math.random() * mul;
+    value.push(rand);
+  }
+  return value;
+}
 
 var trace1 = {
-	x:unpack(rows, 'x1'), y: unpack(rows, 'y1'), z: unpack(rows, 'z1'),
-	mode: 'markers',
-	marker: {
-		size: 12,
-		line: {
-		color: 'rgba(217, 217, 217, 0.14)',
-		width: 0.5},
-		opacity: 0.8},
-	type: 'scatter3d'
+  type: "mesh3d",
+  x: getrandom(1000, 200),
+  y: getrandom(1000, 300),
+  z: getrandom(1000, 150),
+  color: "lightblue"
 };
 
 var trace2 = {
-	x:unpack(rows, 'x2'), y: unpack(rows, 'y2'), z: unpack(rows, 'z2'),
-	mode: 'markers',
-	marker: {
-		color: 'rgb(127, 127, 127)',
-		size: 12,
-		symbol: 'circle',
-		line: {
-		color: 'rgb(204, 204, 204)',
-		width: 1},
-		opacity: 0.8},
-	type: 'scatter3d'};
+  type: "mesh3d",
+  x: getrandom(1000, 200),
+  y: getrandom(1000, 300),
+  z: getrandom(1000, 150),
+  color: "pink"
+};
 
-var data = [trace1, trace2];
-var layout = {margin: {
-	l: 0,
-	r: 0,
-	b: 0,
-	t: 0
-  }};
-Plotly.newPlot('myDiv', data, layout);
+var layout = {
+  scene: {
+    xaxis: { title: "X AXIS TITLE" },
+    yaxis: { title: "Y AXIS TITLE" },
+    zaxis: { title: "Z AXIS TITLE" }
+  },
+  autosize: false,
+  width: 550,
+  height: 500,
+  margin: {
+    l: 0,
+    r: 0,
+    b: 50,
+    t: 50,
+    pad: 4
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+ Plotly.newPlot("myDiv", [trace1, trace2], layout);
 });
+
