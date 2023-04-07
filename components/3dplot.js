46cca4a3e8511b57c9fd66c8794ab4dde23feddf
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-04-06 18:54:06 -0600

    more shapes

--- components/3dplot.js
+++ components/3dplot.js
@@ -1,18 +1,18 @@
- getrandom=(num, mul)=> {
+getrandom = (num, mul) => {
   var value = [];
   for (i = 0; i <= num; i++) {
     var rand = Math.random() * mul;
     value.push(rand);
   }
   return value;
-}
+};
 
 var trace1 = {
   type: "mesh3d",
   x: getrandom(1000, 200),
   y: getrandom(1000, 300),
   z: getrandom(1000, 150),
-  color: "lightblue"
+  color: "lightblue",
 };
 
 var trace2 = {
@@ -20,14 +20,14 @@ var trace2 = {
   x: getrandom(1000, 200),
   y: getrandom(1000, 300),
   z: getrandom(1000, 150),
-  color: "pink"
+  color: "pink",
 };
 
 var layout = {
-  scene: { 
+  scene: {
     xaxis: { title: "X VECTOR" },
     yaxis: { title: "Y VECTOR" },
-    zaxis: { title: "Z VECTOR" }
+    zaxis: { title: "Z VECTOR" },
   },
   autosize: false,
   width: 550,
@@ -37,11 +37,172 @@ var layout = {
     r: 0,
     b: 50,
     t: 50,
-    pad: 4
-  }
+    pad: 4,
+  },
 };
 
 document.addEventListener("DOMContentLoaded", () => {
- Plotly.newPlot("myDiv", [trace1, trace2], layout);
+  Plotly.newPlot("myDiv", [trace1, trace2], layout);
 });
 
+var pointCount = 3142;
+
+var i, r;
+
+var x = [];
+
+var y = [];
+
+var z = [];
+
+var c = [];
+
+for (i = 0; i < pointCount; i++) {
+  r = i * (pointCount - i);
+
+  x.push(r * Math.cos(i / 30));
+
+  y.push(r * Math.sin(i / 30));
+
+  z.push(i);
+
+  c.push(i);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  Plotly.newPlot("myDiv2", [
+    {
+      type: "scatter3d",
+
+      mode: "lines",
+
+      x: x,
+
+      y: y,
+
+      z: z,
+
+      opacity: 0.7,
+
+      line: {
+        width: 10,
+
+        color: c,
+
+        colorscale: "Viridis",
+      },
+    },
+  ]);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  var pointCount = 31;
+
+  var i, r;
+
+  var x = [];
+
+  var y = [];
+
+  var z = [];
+
+  var c = [];
+
+  for (i = 0; i < pointCount; i++) {
+    r = 10 * Math.cos(i / 10);
+
+    x.push(r * Math.cos(i));
+
+    y.push(r * Math.sin(i));
+
+    z.push(i);
+
+    c.push(i);
+  }
+
+  Plotly.newPlot("myDiv3", [
+    {
+      type: "scatter3d",
+
+      mode: "lines+markers",
+
+      x: x,
+
+      y: y,
+
+      z: z,
+
+      line: {
+        width: 6,
+
+        color: c,
+
+        colorscale: "Viridis",
+      },
+
+      marker: {
+        size: 3.5,
+
+        color: c,
+
+        colorscale: "Greens",
+
+        cmin: -20,
+
+        cmax: 50,
+      },
+    },
+  ]);
+});
+
+var data = [
+
+ {
+
+     type: "isosurface",
+
+     x: [0,0,0,0,1,1,1,1],
+
+     y: [0,1,0,1,0,1,0,1],
+
+     z: [1,1,0,0,1,1,0,0],
+
+     value: [1,2,3,4,5,6,7,8],
+
+     isomin: 2,
+
+     isomax: 6,
+
+     colorscale: "Reds"
+
+ }
+
+];
+
+
+var layout = {
+
+ margin: {t:0, l:0, b:0},
+
+ scene: {
+
+     camera: {
+
+         eye: {
+
+             x: 1.88,
+
+             y: -2.12,
+
+             z: 0.96
+
+         }
+
+     }
+
+ }
+
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+Plotly.newPlot('myDiv4', data, layout, {showSendToCloud: true});});
